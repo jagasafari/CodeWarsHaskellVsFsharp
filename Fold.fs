2ea406c4eeb9af30436ccfdae680ff53a2154f18
mika <kaszuba.mika@gmail.com>
2017-04-07 22:13:22 +0200

    improve

--- Fold.fs
+++ Fold.fs
@@ -1,25 +1,10 @@
-type IndexPosition = | Middle | SecondHalf of int | FirstHalf
-
-let getIndexPosition len halfLen = function 
-    | idx when idx < halfLen -> FirstHalf
-    | idx when idx = halfLen && len%2=1 -> Middle
-    | idx -> SecondHalf idx
-
-let foldOnce getIdxPosition array lastIdx =
-    let rec foldOnce (x:xs) acc = function
-        | FirstHalf -> acc
-        | Middle -> acc::[x]
-        | SecondHalf idx -> 
-            foldOnce 
-                xs 
-                (acc::[x+array.[idx]) 
-                (getIdxPosition (idx-1))
-    foldOnce array (Array.empty) lastIdx
-
-let rec foldArray foldOnce array = function
-    | 0 -> array | c -> foldArray (foldOnce array) (c-1)
-    
-let foldArray array runs =
-    let len = Array.lenght array
-    let foldOnce = foldOnce (getIndexPosition len (len/2))
-    foldArray foldOnce array runs
+let foldArray (array: int array) runs =
+    let rec foldOnce = function
+        | (l, r) when l > r -> r
+        | (l, r) when l = r -> l
+        | (l, r) -> array.[l] <- (array.[l] + array.[r]); foldOnce (l+1, r-1)
+    let rec foldNTimes = function
+        | (endIdx, 0) -> array.[0..endIdx]
+        | (endIdx, c) -> foldNTimes (foldOnce (0, endIdx), (c-1))
+    ((Array.length array) - 1, runs) |> foldNTimes 
+printfn "%A" (foldArray [|1;2;3;4;5|] 1)
