7ae769e32e948bb8924e07cb39705c5b056a954d
root <kaszuba.mika@gmail.com>
2017-06-21 16:46:12 +0200

    post param

--- curlPost/Main.hs
+++ curlPost/Main.hs
@@ -1,3 +1,5 @@
 import System.Process
 
-getCommand = createProcess $ shell "curl http://localhost:3000"
+curl = createProcess . shell
+getCommand = curl "curl http://localhost:3000"
+postNameParam = curl "curl --data 'name=mika' http://localhost:3000/simulateFailure"
