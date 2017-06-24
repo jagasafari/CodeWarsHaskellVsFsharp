bd2c2b9cf73bd1e8e79f98f71c0d19ac5d861c99
root <kaszuba.mika@gmail.com>
2017-06-24 23:00:37 +0200

    refactor curl commands

--- curlPost/Main.hs
+++ curlPost/Main.hs
@@ -1,6 +1,15 @@
 import System.Process
 
-curl = createProcess . shell
-getCommand = curl "curl http://localhost:3000"
-postNameParam = curl "curl --data 'name=mika' http://localhost:3000/simulateFailure"
-postJsonData = curl "curl --header 'Content-Type: application/json' --data '{name=mika}' http://localhost:3000/deserializingJsonData "
+data CurlArgs = 
+    UrlOnly String 
+    | WithHeader String String 
+    | WithData String String 
+    | WithDataHeader String String
+
+runCurlCmd a = createProcess . shell . unwords $ "curl":a
+
+curl (UrlOnly args) = runCurlCmd [args]
+
+getCommand = curl $ UrlOnly "http://localhost:3000"
+postNameParam = curl $ UrlOnly "--data 'name=mika' http://localhost:3000/simulateFailure"
+postJsonData = curl $ UrlOnly "--header 'Content-Type: application/json' --data '{name=mika}' http://localhost:3000/deserializingJsonData "
