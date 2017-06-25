1bd0ab5917242e4505aa82846298bc788833e763
root <kaszuba.mika@gmail.com>
2017-06-25 11:49:21 +0200

    key value pair representaion of curl args

--- curlPost/Main.hs
+++ curlPost/Main.hs
@@ -1,18 +1,17 @@
 import System.Process
 
-data CurlArgs = 
-    | UrlOnly String 
-    | WithArgs [String] String 
+data CurlArgs = UrlOnly String | WithArgs [(String, String)] String 
 
 runCurlCmd a = createProcess . shell . unwords $ "curl":a
 
+concatArgs args = map (\(x,y) -> "--" ++ x ++ " " ++ y) args
 curl (UrlOnly url) = runCurlCmd [url]
-curl (WithArgs args url) = runCurlCmd $ args ++ [url]
+curl (WithArgs args url) = runCurlCmd $ concatArgs args ++ [url]
 
 getCommand = curl $ UrlOnly "http://localhost:3000"
-postNameParam = curl $ WithArgs ["--data 'name=mika'"] "http://localhost:3000/simulateFailure"
+postNameParam = curl $ WithArgs [("data", "'name=mika'")] "http://localhost:3000/simulateFailure"
 postJsonData = 
     curl $ WithArgs 
-            ["--header 'Content-Type: application/json'"
-            , "--data '{name=mika}'"] 
+            [("header", "'Content-Type: application/json'")
+            , ("data", "'{name=mika}'")] 
             "http://localhost:3000/deserializingJsonData "
