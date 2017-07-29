e44c13781edd3af569b9c5833f5315772f3af8b0
root <kaszuba.mika@gmail.com>
2017-07-30 00:41:54 +0200

    monad class and instance

--- learnyouahaskell/ExploringMonads.hs
+++ learnyouahaskell/ExploringMonads.hs
@@ -6,3 +6,16 @@ applyMaybe Nothing _ = Nothing
 applyMaybe (Just x) f = f x
 bothJust = (Just 6) `applyMaybe` (\x -> Just (+8))
 withNothing = Nothing `applyMaybe` (\x -> Just (++ ":)"))
+
+class MyMonad m where
+    return :: a -> m a
+    (.>>=) :: m a -> (a -> m b) -> m b
+    (.>>) :: m a -> m b -> m b
+    x .>> y = x .>>= (\_ -> y)
+    fail :: String -> m a
+    fail msg = error msg
+instance MyMonad Maybe where
+    return x = Just x
+    Nothing .>>= f = Nothing
+    Just x .>>= f = f x
+    fail _ = Nothing
