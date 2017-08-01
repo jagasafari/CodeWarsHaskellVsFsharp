c8d31825ddd51939af24c1aebdbf2ef35cc3cbc6
root <kaszuba.mika@gmail.com>
2017-08-01 18:56:25 +0200

    log writer function

--- learnyouahaskell/ExploringMonads.hs
+++ learnyouahaskell/ExploringMonads.hs
@@ -117,3 +117,6 @@ monoidicCompositionIdentity f = do
     b <- return C.<=< f $ 7
     c <- f 7
     return ((a == b) && (b == c))
+myWriter :: (a, String) -> (a -> (b, String)) -> (b, String)
+myWriter (x, oldLog) f =
+    let (z, newLog) = f x in (z, oldLog ++ newLog)
